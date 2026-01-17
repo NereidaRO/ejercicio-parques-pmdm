2ec18bb2ffed6ed15c45eb58f31179fd1ac8dc87
NereidaRO <nereidarodriguezorenes@gmail.com>
2026-01-17 17:12:38 +0100

    added Room in gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -46,7 +48,20 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    // --- ROOM ---
+    val roomVersion = "2.6.1"
+    // Runtime
+    implementation("androidx.room:room-runtime:$roomVersion")
+    // Procesador de anotaciones para Room (KAPT)
+    kapt("androidx.room:room-compiler:$roomVersion")
+    // Extensión de Kotlin para Room (incluye soporte para corrutinas)
+    implementation("androidx.room:room-ktx:$roomVersion")
+    // ------------
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+}
+kapt {
+    correctErrorTypes = true
 }
